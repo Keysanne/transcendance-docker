8e1a9360bf65857c36016f44948edbb1362cc959
Baptiste Burguiere <baptiste.burguiere@viacesi.fr>
2024-01-05 17:23:08 +0100

    navbar

--- images/vuejs/front/src/components/Navbar.vue
+++ images/vuejs/front/src/components/Navbar.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <nav class="navbar navbar-expand-lg bg-primary fixed-top" data-bs-theme="dark">
     <div class="container-fluid">
       <router-link to="/" class="navbar-brand">Game</router-link>
@@ -28,4 +28,43 @@ export default {
         }
     },
 }
+</script> -->
+
+<template>
+  <div class="fixed-top">
+    <div class="bg-white">
+      <nav class="container px-6 py-3 mx-auto md:flex md:justify-between md:items-center">
+        <div class="flex items-center justify-between">
+          <router-link to="/" class="text-xl font-bold text-gray-800 md:text-2xl hover:text-gray-500 no-underline ease-in-out transition-colors">Home</router-link>
+
+          <!-- Mobile menu button -->
+          <div @click="showMenu = !showMenu" class="flex md:hidden">
+            <button type="button" class="text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-400">
+              <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <ul :class="showMenu ? 'flex' : 'hidden'" class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0 my-0 py-0">
+          <li><router-link class="text-lg font-semibold text-gray-800 hover:text-gray-500 no-underline ease-in-out transition-colors" to="/leaderboard">Leaderboard</router-link></li>
+          <li><router-link class="text-lg font-semibold text-gray-800 hover:text-gray-500 no-underline ease-in-out transition-colors" to="/tournaments">Tournaments</router-link></li>
+          <li class="md:hidden"><router-link class="text-lg font-semibold text-gray-800 hover:text-gray-500 no-underline ease-in-out transition-colors" to="/account">My Account</router-link></li>
+          <li class="hidden md:block"><router-link to="/account"><img class="rounded-full h-14 w-14 border-1 border-black" src="../assets/avatars/todo.jpg" alt="profile_pic"></router-link></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+};
 </script>
--- images/vuejs/front/src/views/Home.vue
+++ images/vuejs/front/src/views/Home.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="d-flex flex-column align-items-center justify-center min-h-screen bg-[url(https://integraales.fr/wp-content/uploads/2019/12/source.gif)] bg-no-repeat bg-cover">
-        <!-- <Navbar /> -->
+        <Navbar />
         
         <h1 class="text-white text-6xl">Welcome to transcendance!</h1>
         <h3 class="text-gray-300">The best pong game in the world</h3>
-        <h4 class="text-gray-500 text-sm">(GOTY 2024)</h4>
+        <h4 class="text-white opacity-25 text-sm">(GOTY 2024)</h4>
 
         <div class="d-grid gap-3 col-3 mx-auto mt-20">
             <button class="btn btn-light btn-lg" type="button">Play</button>
