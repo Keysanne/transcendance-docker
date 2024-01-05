777ddb483e76137a37275e205fba23736d8d119c
Baptiste Burguiere <baptiste.burguiere@viacesi.fr>
2024-01-05 17:12:40 +0100

    404 not found

--- images/vuejs/front/src/router/index.js
+++ images/vuejs/front/src/router/index.js
@@ -7,6 +7,7 @@ import Tournaments from '../views/Tournaments.vue'
 import TournamentView from '../views/TournamentView.vue'
 import Matchmaking from '../views/Matchmaking.vue'
 import Home from '../views/Home.vue'
+import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -18,6 +19,7 @@ const router = createRouter({
         {path: '/tournaments', name: 'Tournaments', component: Tournaments},
         {path: '/tournament/:id', name: 'TournamentView', component: TournamentView},
         {path: '/matchmaking', name: 'Matchmaking', component: Matchmaking},
+        {path: '/:notFound', component: NotFound},  
     ]
 })
 
---
+++ images/vuejs/front/src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="d-flex flex-column align-items-center justify-center min-h-screen bg-[url(https://integraales.fr/wp-content/uploads/2019/12/source.gif)] bg-no-repeat bg-cover">
+        <Navbar />
+        <h1 class="text-white text-8xl">404 not found</h1>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+
+export default {
+    components: {
+        Navbar,
+    }
+}
+</script>
