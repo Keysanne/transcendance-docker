950b178cf8a93cfe4bb6113ace7627eb5d9283eb
Baptiste Burguiere <baptiste.burguiere@viacesi.fr>
2024-01-11 14:37:58 +0100

    fix responsive main pages

--- images/vuejs/front/src/views/Play.vue
+++ images/vuejs/front/src/views/Play.vue
@@ -2,9 +2,9 @@
     <div class="d-flex flex-column align-items-center justify-center min-h-screen bg-[url(https://integraales.fr/wp-content/uploads/2019/12/source.gif)] bg-no-repeat bg-cover">
         <Navbar />
         
-        <h1 class="text-white text-6xl">Choose your game mode</h1>
+        <h1 class="text-white text-4xl md:text-5xl lg:text-6xl text-center">Choose your game mode</h1>
 
-        <div class="d-grid gap-3 col-3 mx-auto mt-20">
+        <div class="d-grid gap-3 col-6 col-md-3 mx-auto mt-20">
             <router-link to="/difficulty" class="btn btn-light btn-lg">Solo vs AI</router-link>
 			<router-link to="/pong" class="btn btn-light btn-lg">Local 1 vs 1</router-link>
         </div>
