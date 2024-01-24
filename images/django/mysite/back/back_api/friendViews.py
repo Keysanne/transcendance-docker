4d7cd273c79ac8806a7029a06d2d2654439739d4
Andrea <andrea.walter03@gmail.com>
2024-01-24 14:35:46 +0100

    cleaned Update and removed patch and post

--- images/django/mysite/back/back_api/friendViews.py
+++ images/django/mysite/back/back_api/friendViews.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .models import User, Friend
 from rest_framework import generics
 from .serializers import UserSerializer, FriendSerializer
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
